9eb8182ee3fb11e8209ddade06ca17d91180931d
indium731 <alfredivarsso@gmail.com>
2026-04-30 13:21:48 +0200

    när man ska välja spel att boka så visas enbart tillgängliga spel

--- BokningRelaterat/BokaSpelVy.xaml.cs
+++ BokningRelaterat/BokaSpelVy.xaml.cs
@@ -11,14 +11,39 @@ namespace Labb1_OOP
         public BokaSpelVy(Bokning bokning)
         {
             InitializeComponent();
-            UppdateraUI();
             BokaSpelKnapp.Tag = bokning;
+            InitieraLista();
+            UppdateraUI();
+        }
+
+        private void InitieraLista()
+        {
+            //Jag ber om ursäkt för denna synd
+            //  men vad koden gör är att den separerar ut om några tidigare bokningar överlappar
+            //  tidsmässigt med en man försöker lägga.
+            //  Sedan tar den reda på om någon av dessa tidigare,
+            //  överlappande bokningarna har bokat upp ett givet spel.
+            //  Om spelet inte är bokat under denna tid så läggs den till i listan av spel som kan bokas.
+
+            SpelListaLada.ItemsSource = null;
+            List<Bokning> overlappandeBokningar = (Bokningar.HamtaBokningar().bokningar.Where(bokning => bokning.startDatum < ((Bokning)BokaSpelKnapp.Tag).slutDatum && ((Bokning)BokaSpelKnapp.Tag).startDatum < bokning.slutDatum)).ToList();
+            List<Spel> tillgangligaSpel = new List<Spel>();
+            foreach (Spel spel in SpelLista.HamtaSpelLista().spel)
+            {
+                if (overlappandeBokningar.Where(bokning => bokning.bokadeSpel.Contains(spel)).Count() == 0)
+                {
+                    tillgangligaSpel.Add(spel);
+                }
+            }
+            SpelListaLada.ItemsSource = tillgangligaSpel;
         }
 
         private void UppdateraUI()
         {
+            var lista = SpelListaLada.ItemsSource;
             SpelListaLada.ItemsSource = null;
-            SpelListaLada.ItemsSource = SpelLista.HamtaSpelLista().spel;
+            SpelListaLada.ItemsSource = lista;
+
         }
 
         private void GaTillMeny(Object sender, RoutedEventArgs e)
