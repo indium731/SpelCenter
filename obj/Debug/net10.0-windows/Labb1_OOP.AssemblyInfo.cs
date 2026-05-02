2fc541ab8c38a905a796f5cfb7b2cc10056d7784
indium731 <alfredivarsso@gmail.com>
2026-05-02 15:50:50 +0200

    går nu att uppdatera medlemsinformation exklusive medlemskapet

--- obj/Debug/net10.0-windows/Labb1_OOP.AssemblyInfo.cs
+++ obj/Debug/net10.0-windows/Labb1_OOP.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Labb1_OOP")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8baf5e6afd39fa4760e77f5f12fa608e03478280")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9eb8182ee3fb11e8209ddade06ca17d91180931d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Labb1_OOP")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Labb1_OOP")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
