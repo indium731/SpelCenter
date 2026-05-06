a7c8087dc448b4a887dfe6daa2b225f3b5ba54d1
Alexadeln <S2509406@student.hb.se>
2026-05-06 12:25:36 +0200

    Påbörjat bokningar

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -29,12 +29,16 @@ public partial class MainWindow : Window
 
 		MedlemsLista.HamtaMedlemsLista().medlemmar.Add(new Medlem("Alexander", "0701234567", "S1", true));   // admin
 		MedlemsLista.HamtaMedlemsLista().medlemmar.Add(new Medlem("Lisa", "0739876543", "S2", false));      // vanlig
-		MedlemsLista.HamtaMedlemsLista().medlemmar.Add(new Medlem("Johan", "0725551234", "S3", false));
+		MedlemsLista.HamtaMedlemsLista().medlemmar.Add(new Medlem("Pelle", "0725551234", "S3", false));
 		MedlemsLista.HamtaMedlemsLista().medlemmar.Add(new Medlem("Emma", "0761112233", "S4", true));       // admin
 
 		SpelLista.HamtaSpelLista().spel.Add(new Spel("Uno", "Sällskap", 2, 8, Svarighetsgrad.barnvänligt , "kortspel"));
+		SpelLista.HamtaSpelLista().spel.Add(new Spel("Fyra i rad", "Sällskap", 2, 2, Svarighetsgrad.barnvänligt, "Få fyra i rad"));
+		SpelLista.HamtaSpelLista().spel.Add(new Spel("Call of duty", "Strategi", 2, 8, Svarighetsgrad.barnvänligt, "Actionfyllt strategispel"));
+		SpelLista.HamtaSpelLista().spel.Add(new Spel("Guitarherokortspelet", "Sällskap", 1, 2, Svarighetsgrad.barnvänligt, "kortspel"));
 
-		Bokningar().HamtaBokningar().bokningar.Add(new Bokning(DateTime d, DateTime s, string p, int m, Medlem a, string b));
+
+		Bokningar().HamtaBokningar().bokningar.Add(new Bokning(new DateTime (2024, 03, 12),new  DateTime (2024, 03, 12), "Majorna", 5, Medlem a, string b));
 	}
 
 }
