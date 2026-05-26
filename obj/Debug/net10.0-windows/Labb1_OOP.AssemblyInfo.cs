6d2b5271ff74981674557c3c149fa7cb4c259f95
indium731 <alfredivarsso@gmail.com>
2026-05-26 18:51:27 +0200

    bytt plats på beskrivning och svårighetsgrad input lådor

--- obj/Debug/net10.0-windows/Labb1_OOP.AssemblyInfo.cs
+++ obj/Debug/net10.0-windows/Labb1_OOP.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Labb1_OOP")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1c36c14740e4cba28cbc099e670906c0699a0ace")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b51e391a058b049c8a2a9e007e19801662a88982")]
 [assembly: System.Reflection.AssemblyProductAttribute("Labb1_OOP")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Labb1_OOP")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
