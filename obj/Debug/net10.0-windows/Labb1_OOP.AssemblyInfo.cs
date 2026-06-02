f7f6b55e09ea83eb60baec74114de3acef7b61ac
indium731 <alfredivarsso@gmail.com>
2026-06-02 11:10:22 +0200

    tagit bort IListBar då den inte används

--- obj/Debug/net10.0-windows/Labb1_OOP.AssemblyInfo.cs
+++ obj/Debug/net10.0-windows/Labb1_OOP.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Labb1_OOP")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9288502e63fa79a063a1e84c66995229babb7b07")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bb8e1906fdd11452d72bb6a0ce61bfb30c918c56")]
 [assembly: System.Reflection.AssemblyProductAttribute("Labb1_OOP")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Labb1_OOP")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
